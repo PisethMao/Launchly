9436e18e12b3acc5d2b402d8eccf37c92e89fe61
PisethMao <pisethmao2002@gmail.com>
2025-11-06 12:38:09 +0700

    finished 100% of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import HeroSection from "@/components/hero_section/HeroSection";
 import DeploymentPreview from "@/components/home/DeploymntPreview";
 import HighLight from "@/components/home/HighLight";
 import HowItWorks from "@/components/home/HowItWorks";
+import BeforeAfterSection from "@/components/landing/BeforeAfterSection";
+import TargetUsersSection from "@/components/landing/TargetUsersSection";
 import TestimonialsSection from "@/components/landing/TestimonialsSection";
 
 export default function Home() {
@@ -10,6 +12,8 @@ export default function Home() {
       <HeroSection />
       <HowItWorks />
       <HighLight />
+      <BeforeAfterSection />
+      <TargetUsersSection />
       <TestimonialsSection />
       <DeploymentPreview />
     </main>
